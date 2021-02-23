c2b0f4d47b80b33592b7a753422302b87b1afc98
mytschez <72097976+mytschez@users.noreply.github.com>
2021-02-23 20:06:33 +0100

    Add files via upload

---
+++ VisorImagenes.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmVisorImagenes
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PicBox = New System.Windows.Forms.PictureBox()
+        Me.btnAnterior = New System.Windows.Forms.Button()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.btnSiguiente = New System.Windows.Forms.Button()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        Me.FBDCarpetaFotos = New System.Windows.Forms.FolderBrowserDialog()
+        Me.TimerFotos = New System.Windows.Forms.Timer(Me.components)
+        Me.Panelbtns = New System.Windows.Forms.Panel()
+        Me.btnLupa = New System.Windows.Forms.Button()
+        CType(Me.PicBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panelbtns.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PicBox
+        '
+        Me.PicBox.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PicBox.Location = New System.Drawing.Point(0, 0)
+        Me.PicBox.Name = "PicBox"
+        Me.PicBox.Size = New System.Drawing.Size(1209, 579)
+        Me.PicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBox.TabIndex = 0
+        Me.PicBox.TabStop = False
+        '
+        'btnAnterior
+        '
+        Me.btnAnterior.Enabled = False
+        Me.btnAnterior.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAnterior.Font = New System.Drawing.Font("Webdings", 26.0!, System.Drawing.FontStyle.Bold)
+        Me.btnAnterior.Location = New System.Drawing.Point(0, 1)
+        Me.btnAnterior.Name = "btnAnterior"
+        Me.btnAnterior.Size = New System.Drawing.Size(90, 90)
+        Me.btnAnterior.TabIndex = 1
+        Me.btnAnterior.Text = "7"
+        Me.btnAnterior.UseVisualStyleBackColor = True
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Enabled = False
+        Me.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay.Font = New System.Drawing.Font("Webdings", 26.0!, System.Drawing.FontStyle.Bold)
+        Me.btnPlay.Location = New System.Drawing.Point(106, 1)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(90, 90)
+        Me.btnPlay.TabIndex = 2
+        Me.btnPlay.Text = "4"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'btnSiguiente
+        '
+        Me.btnSiguiente.Enabled = False
+        Me.btnSiguiente.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSiguiente.Font = New System.Drawing.Font("Webdings", 26.0!, System.Drawing.FontStyle.Bold)
+        Me.btnSiguiente.Location = New System.Drawing.Point(212, 1)
+        Me.btnSiguiente.Name = "btnSiguiente"
+        Me.btnSiguiente.Size = New System.Drawing.Size(90, 90)
+        Me.btnSiguiente.TabIndex = 3
+        Me.btnSiguiente.Text = "8"
+        Me.btnSiguiente.UseVisualStyleBackColor = True
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.IndianRed
+        Me.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCerrar.Font = New System.Drawing.Font("Webdings", 26.0!, System.Drawing.FontStyle.Bold)
+        Me.btnCerrar.Location = New System.Drawing.Point(1107, 587)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(90, 90)
+        Me.btnCerrar.TabIndex = 4
+        Me.btnCerrar.Text = "r"
+        Me.btnCerrar.UseVisualStyleBackColor = True
+        '
+        'FBDCarpetaFotos
+        '
+        Me.FBDCarpetaFotos.ShowNewFolderButton = False
+        '
+        'TimerFotos
+        '
+        Me.TimerFotos.Interval = 3000
+        '
+        'Panelbtns
+        '
+        Me.Panelbtns.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Panelbtns.Controls.Add(Me.btnPlay)
+        Me.Panelbtns.Controls.Add(Me.btnSiguiente)
+        Me.Panelbtns.Controls.Add(Me.btnAnterior)
+        Me.Panelbtns.Location = New System.Drawing.Point(433, 586)
+        Me.Panelbtns.Name = "Panelbtns"
+        Me.Panelbtns.Size = New System.Drawing.Size(303, 91)
+        Me.Panelbtns.TabIndex = 1
+        '
+        'btnLupa
+        '
+        Me.btnLupa.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnLupa.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSkyBlue
+        Me.btnLupa.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLupa.Font = New System.Drawing.Font("Webdings", 26.0!, System.Drawing.FontStyle.Bold)
+        Me.btnLupa.Location = New System.Drawing.Point(12, 586)
+        Me.btnLupa.Name = "btnLupa"
+        Me.btnLupa.Size = New System.Drawing.Size(90, 90)
+        Me.btnLupa.TabIndex = 0
+        Me.btnLupa.Text = "L"
+        Me.btnLupa.UseVisualStyleBackColor = True
+        '
+        'frmVisorImagenes
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1209, 695)
+        Me.Controls.Add(Me.btnLupa)
+        Me.Controls.Add(Me.Panelbtns)
+        Me.Controls.Add(Me.btnCerrar)
+        Me.Controls.Add(Me.PicBox)
+        Me.MinimumSize = New System.Drawing.Size(650, 450)
+        Me.Name = "frmVisorImagenes"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Visor de imágenes"
+        CType(Me.PicBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panelbtns.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PicBox As PictureBox
+    Friend WithEvents btnAnterior As Button
+    Friend WithEvents btnPlay As Button
+    Friend WithEvents btnSiguiente As Button
+    Friend WithEvents btnCerrar As Button
+    Friend WithEvents FBDCarpetaFotos As FolderBrowserDialog
+    Friend WithEvents TimerFotos As Timer
+    Friend WithEvents Panelbtns As Panel
+    Friend WithEvents btnLupa As Button
+End Class
---
+++ VisorImagenes.vb
@@ -0,0 +1,139 @@
+﻿Imports System.IO
+
+Public Class frmVisorImagenes
+    'variables
+    Dim listaFotos() As String 'vector para guardar cada una de las fotos de la carpeta
+    Dim carpeta As String 'para cargar la ruta de la carpeta
+    Dim longvector As Integer 'para recoger el tamaño del array puesto que es dinámico
+    Dim i As Integer
+    Dim play As Boolean = False
+    Dim foto As Integer
+
+    'BUSCAR
+    Private Sub btnLupa_Click(sender As Object, e As EventArgs) Handles btnLupa.Click
+        CargarFotos()
+    End Sub
+
+    'CERRAR
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        Application.Exit()
+    End Sub
+
+    'SIGUIENTE
+    Private Sub btnSiguiente_Click(sender As Object, e As EventArgs) Handles btnSiguiente.Click
+        play = False
+        TimerFotos.Stop()
+        AntSigFoto("siguiente")
+    End Sub
+    'ANTERIOR
+    Private Sub btnAnterior_Click(sender As Object, e As EventArgs) Handles btnAnterior.Click
+        play = False
+        TimerFotos.Stop()
+        AntSigFoto("anterior")
+    End Sub
+    'TIMER
+    Private Sub TimerFotos_Tick(sender As Object, e As EventArgs) Handles TimerFotos.Tick
+        AntSigFoto("siguiente")
+    End Sub
+    'PLAY
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        play = Not play
+        If play Then
+            TimerFotos.Start()
+            btnPlay.Text = ";"
+        Else
+            TimerFotos.Stop()
+            btnPlay.Text = "4"
+        End If
+    End Sub
+    'REAJUSTAR LOS BOTONES
+    Private Sub frmVisorImagenes_ResizeEnd(sender As Object, e As EventArgs) Handles MyBase.ResizeEnd
+        resizeForm()
+    End Sub
+    'procedimiento para cargar las fotos
+    Private Sub CargarFotos()
+        Try
+            If FBDCarpetaFotos.ShowDialog() = DialogResult.OK Then
+                carpeta = FBDCarpetaFotos.SelectedPath ' guardamos la ruta
+                longvector = Directory.GetFiles(carpeta, "*.jpg").Length 'longitud del vector
+
+                'redimensionar el vector con el tamaño
+                ReDim listaFotos(longvector - 1)
+
+                'habilitar botones
+                If longvector > 0 Then
+                    btnAnterior.Enabled = True
+                    btnPlay.Enabled = True
+                    btnSiguiente.Enabled = True
+                End If
+
+                i = 0 'inicializamos el índice a cero
+
+                'Directory.GetFiles(carpeta, "*jpg") coleccionable con la ruta de todas 
+                'las fotos de la carpeta con formato .jpg
+                For Each archivo In Directory.GetFiles(carpeta, "*jpg")
+                    listaFotos(i) = archivo
+                    i += 1
+                Next
+                i = 0
+                PicBox.Load(listaFotos(i)) 'cargar las fotos en el pictureBox
+            End If
+        Catch ex As Exception
+            MessageBox.Show("La carpeta seleccionada no contiene ninguna imagen JPG", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    'procedimiento para pasar de foto hacia adelante o hacia atrás
+    Private Sub AntSigFoto(pasar As String)
+
+        If pasar = "siguiente" Then
+            'si es la última foto vuelve a mostrar la primera
+            If foto = longvector - 1 Then
+                foto = 0
+            Else
+                foto += 1
+            End If
+        Else
+            If pasar = "anterior" Then
+                'si es la primera foto vuelve a mostrar la última
+                If foto = 0 Then
+                    foto = longvector - 1
+                Else
+                    foto -= 1
+                End If
+            End If
+        End If
+        PicBox.Load(listaFotos(foto))
+    End Sub
+
+    'procedimiento para reajustar los botones si se modifica el tamaño del formulario
+    Private Sub resizeForm()
+        'variables
+        Dim posicion As Point
+        Dim AnchoForm As Integer = Me.Width
+        Dim AltoForm As Integer = Me.Height
+        Dim AnchoPanel As Integer = Panelbtns.Width
+        Dim AltoPanel As Integer = Panelbtns.Height
+        Dim AnchobtnCerrar As Integer = btnCerrar.Width
+
+        'reajuste de los botones anterior, play y siguiente
+        posicion.X = (AnchoForm - AnchoPanel) / 2
+        posicion.Y = AltoForm - (50 + AltoPanel)
+        Panelbtns.Location = posicion
+
+        'reajuste del botón buscar
+        posicion.X = 12
+        btnLupa.Location = posicion
+
+        'reajuste del botón cerrar
+        posicion.X = AnchoForm - (AnchobtnCerrar + 30)
+        btnCerrar.Location = posicion
+
+        'reajuste del picture box
+        PicBox.Height = AltoForm - 120
+        PicBox.Width = AnchoForm
+
+    End Sub
+
+End Class
